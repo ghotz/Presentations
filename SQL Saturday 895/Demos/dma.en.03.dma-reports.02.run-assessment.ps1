5a14bea2d226ff1d660efbcf7dff8fe7c8a698e7
Gianluca Hotz <gianluca_hotz@hotmail.com>
2019-11-24 18:21:51 +0100

    Add files via upload

---
+++ SQL Saturday 895/Demos/dma.en.03.dma-reports.02.run-assessment.ps1
@@ -0,0 +1,29 @@
+﻿#
+# Consolidate assessment report
+# Note: install everything as described in https://docs.microsoft.com/en-us/sql/dma/dma-consolidatereports
+# Errors logged in C:\Users\gianl\AppData\Local\DataMigrationAssistant
+#
+
+
+# run assessment against all configure instances
+dmaDataCollector `
+    -getServerListFrom SqlServer `
+    -serverName localhost `
+    -databaseName EstateInventory `
+    -AssessmentName "Demo Assessment" `
+    -TargetPlatform AzureSqlDatabase `
+    -OutputLocation C:\temp\dma\consolidated\ `
+    -AuthenticationMethod WindowsAuth
+
+# import data into database
+dmaProcessor `
+    -processTo SQLServer `
+    -serverName localhost `
+    -CreateDMAReporting 1 `
+    -CreateDataWarehouse 1 `
+    -databaseName DMAReporting `
+    -warehouseName DMAWarehouse `
+    -jsonDirectory C:\temp\dma\consolidated\
+
+
+Invoke-Sqlcmd -ServerInstance localhost -InputFile "C:\Users\gianl\OneDrive\Documents\Presentations\Topics\Demos\migrations\LoadWarehouse.sql"
